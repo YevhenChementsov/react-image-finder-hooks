70834ea58317e6aced29987acd703238abaa6a27
Yevhen Chementsov <yevhenchementsov@gmail.com>
2023-09-13 18:57:23 +0300

    Refactor SearchBar.jsx using hooks

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,51 +1,48 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { ReactComponent as SearchIcon } from '@/icons/search.svg';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import { Button, Form, Header, Input } from './SearchBar.styled';
-export default class SearchBar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
 
-  state = {
-    value: '',
-  };
+const SearchBar = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
 
-  handleValueChange = ({ target: { value } }) => {
-    this.setState({ value });
-  };
+  const handleValueChange = ({ target: { value } }) => setValue({ value });
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.value.trim() === '') {
+    if (value.trim() === '') {
       return toast.warning('No query entered yet...');
     }
 
-    this.props.onSubmit(this.state.value);
-    this.setState({ value: '' });
+    onSubmit(value);
+    setValue({ value: '' });
   };
 
-  render() {
-    return (
-      <Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Button type="submit" aria-label="Search-button">
-            <SearchIcon width="16" height="16" />
-          </Button>
-          <Input
-            autoComplete="off"
-            autoFocus
-            name="value"
-            onChange={this.handleValueChange}
-            placeholder="Search images and photos"
-            type="text"
-            value={this.state.value}
-          />
-        </Form>
-      </Header>
-    );
-  }
-}
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit" aria-label="Search-button">
+          <SearchIcon width="16" height="16" />
+        </Button>
+        <Input
+          autoComplete="off"
+          autoFocus
+          name="value"
+          onChange={handleValueChange}
+          placeholder="Search images and photos"
+          type="text"
+          value={value}
+        />
+      </Form>
+    </Header>
+  );
+};
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
